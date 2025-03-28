8671365de54514bda81f65ce562f7258d0de667e
Zack <97266950+zackadoodledoo@users.noreply.github.com>
2025-03-28 11:21:20 -0700

    Update test_smokeTest.py

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -3,6 +3,7 @@ import pytest
 import time
 import json
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
